a8e268060e2a98bd121f558099a9bcf11df7628c
Iswar <rick731202@gmail.com>
2022-05-25 08:55:37 +0530

    day 2 on the pokemon page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,33 @@ import Pokemon from './pages/Pokemon';
 function App() {
   return (
     <>
-      <div className='min-h-screen bg-slate-100 dark:bg-slate-800'>
+      <main className='min-h-screen bg-slate-100 dark:bg-slate-800 overflow-x-hidden'>
         <Routes>
           <Route path='/' element={<Dashboard />} />
-          <Route path='/pokemon' element={<Pokemon />} />
+          <Route path='/pokemon' element={<Pokemon pokemonData={
+            {
+              "abilities": [
+                {
+                "ability": {
+                "name": "overgrow",
+                "url": "https://pokeapi.co/api/v2/ability/65/"
+                },
+                "is_hidden": false,
+                "slot": 1
+                },
+                {
+                "ability": {
+                "name": "chlorophyll",
+                "url": "https://pokeapi.co/api/v2/ability/34/"
+                },
+                "is_hidden": true,
+                "slot": 3
+                }
+                ],
+            }
+          } />} />
         </Routes>
-      </div>
+      </main>
     </>
   );
 }
--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PokemonAvatar from '../components/PokemonAvatar'
-import TopTitleBar from '../components/TopTitleBar'
 
-function Pokemon() {
+function Pokemon(props) {
   return (
-    <>
-        <TopTitleBar />
+    <div className='relative flex flex-col items-center'>
         <PokemonAvatar src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"} name={'Bulbasaur'}/>
-    </>
+        <section className='z-10 absolute top-[34vh] bg-slate-50 p-4 pokemon-info overflow-x-hidden'>
+          {JSON.stringify(props.pokemonData)}
+        </section>
+    </div>
   )
 }
 
